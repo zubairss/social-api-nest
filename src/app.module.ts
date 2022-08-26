5a0a132864a4f6378ef8472b6a255d75b1ad601f
Zubair <zubair@jawads-MacBook-Pro.local>
2022-08-26 09:34:04 +0500

    added modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AuthModule } from './auth/auth.module';
+import { UserModule } from './user/user.module';
+import { PostModule } from './post/post.module';
 
 @Module({
-  imports: [],
+  imports: [AuthModule, UserModule, PostModule],
   controllers: [AppController],
   providers: [AppService],
 })
