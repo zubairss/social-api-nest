966b48daff46a09c2fdf37102ff018bee5f8c43c
Zubair <zubair@jawads-MacBook-Pro.local>
2022-08-26 09:38:26 +0500

    Added Index Routes

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,7 +1,14 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
 import { AuthService } from './auth.service';
 
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
+
+  @Get('')
+  index(): string{
+    return "Auth Route"
+  }
+
+
 }
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
+
 async function bootstrap() {
+  let port = 3000;
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  await app.listen(port);
+  console.log(`Listening on ${port}`)
 }
 bootstrap();
