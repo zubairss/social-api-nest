acb5d8eae669236cc3b95683a6b7d65a4c496c70
Zubair <zubair@jawads-MacBook-Pro.local>
2022-08-26 10:39:40 +0500

    implemented swagger

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,21 @@
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 
 async function bootstrap() {
   let port = 3000;
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('Social API')
+    .setDescription('Functioanalities - Users, Posts, Auth, CRUD')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
+
   await app.listen(port);
   console.log(`Listening on ${port}`)
 }
