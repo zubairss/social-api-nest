12a86f4ba093660c6c1cd43e0cba6b001f9886db
Zubair <zubair@jawads-MacBook-Pro.local>
2022-08-31 10:44:04 +0500

    Jwt accessToken send with auth

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -2,9 +2,13 @@ import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
 import { UserModule } from 'src/user/user.module';
+import { JwtModule } from '@nestjs/jwt';
 
 @Module({
-  imports: [UserModule],
+  imports: [UserModule, JwtModule.register({
+    secret: "CitrusBits",
+    signOptions: {expiresIn: '1000s'}
+  })],
   controllers: [AuthController],
   providers: [AuthService]
 })
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,4 @@
-import { HttpException, HttpStatus, Injectable, InternalServerErrorException } from '@nestjs/common';
+import { HttpCode, HttpException, HttpStatus, Injectable, InternalServerErrorException } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 import { UserModule } from 'src/user/user.module';
@@ -6,29 +6,48 @@ import { User, UserDocument } from 'src/user/user.schema';
 import { CreateUserDto } from './dto/create-user.dto';
 import { LoginUserDto } from './dto/login-user.dto';
 import * as bcrypt from 'bcrypt';
+import { JwtService } from '@nestjs/jwt';
+
 
 @Injectable()
 export class AuthService {
 
-    constructor(@InjectModel(User.name) private userModel: Model<UserDocument>) {}
+    constructor(@InjectModel(User.name) private userModel: Model<UserDocument>, private jwtService: JwtService) {}
 
-    async registerUser(createUserDto: CreateUserDto): Promise<User> {   
+    async registerUser(createUserDto: CreateUserDto): Promise<any> {   
         const createdUser = new this.userModel(createUserDto);  
         try{
-            return await createdUser.save();
+            await createdUser.save();
+            const payload = { _id: createdUser._id, name: createdUser.name, email: createdUser.email, gender: createdUser.gender, friends: createdUser.friends, friendRequests: createdUser.friendRequests };
+            const access_token = this.jwtService.sign(payload);
+            return {
+                user: payload,
+                accessToken: access_token,
+                message: "User Successfully Created",
+                statusCode: HttpStatus.CREATED
+            }
         } catch(error){
             // throw new InternalServerErrorException(error.message);
             throw new HttpException(error.message, HttpStatus.CONFLICT);   
          }
     }
 
-    async loginUser(loginUserDto: LoginUserDto): Promise<User>{
+    async loginUser(loginUserDto: LoginUserDto): Promise<any>{
 
         return await this.userModel.findOne({ email: loginUserDto.email }).lean().then(async (res) => {
             if (res){
                 const isMatch = await bcrypt.compare(loginUserDto.password, res.password.toString());
                 if(isMatch) {
-                    return res;
+
+                    const payload = { _id: res._id, name: res.name, email: res.email, gender: res.gender, friends: res.friends, friendRequests: res.friendRequests }
+                    const access_token = this.jwtService.sign(payload);
+
+                    return {
+                        user: payload,
+                        accessToken: access_token,
+                        message: "Login Successful",
+                        statusCode: HttpStatus.OK
+                    };
                 }
                 throw new HttpException("Incorrect Password", HttpStatus.UNAUTHORIZED)
             }
